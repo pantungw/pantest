4919ecd15a8e65c1d9253ab3d5c4c495924c2a51
Pantung Wijaya <pantungw@gmail.com>
2011-07-01 11:31:22 -0700

    Initial app setup using RubyMine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pantest::Application.config.secret_token = '90c707a917940b28784c63e81b1d9ff55e7b743c3414450f6b1c8d372f35533bc6e19ea932c34b1bc7d85346ea3b720f5acb84dd98856bfa26bd27dfb2b6c5e0'
